d5f6d319426acc732db53d6c7f94c7c14b3e22be
Saras <78587510+sarasverma@users.noreply.github.com>
2022-03-24 17:21:51 +0530

    skeleton ready

---
+++ src/components/News.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
+
+export default class News extends Component {
+  render() {
+    return (
+      <div>
+        <NewsItem />
+        <NewsItem />
+        <NewsItem />
+        <NewsItem />
+        <NewsItem />
+        <NewsItem />
+      </div>
+    );
+  }
+}
---
+++ src/components/NewsItem.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class NewsItem extends Component {
+  render() {
+    return <div>This is News.</div>;
+  }
+}
+
+export default NewsItem;
